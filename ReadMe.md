8f7c067526684d1640a4f86286f6231b0518b11e
Ben Williams <codebencode@icloud.com>
2024-12-06 10:50:18 +0000

    Add function for name error and started ReadMe

---
+++ ReadMe.md
@@ -0,0 +1,3 @@
+Useful Code for future reference:
+
+- if(!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)) = name does not include letters, one space and more letters (full name)
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,39 @@
-const nameError = document.getElementById('name-error');
-const phoneError = document.getElementById('phone-error');
-const emailError = document.getElementById('email-error');
-const messageError = document.getElementById('message-error');
+let nameError = document.getElementById('name-error');
+let phoneError = document.getElementById('phone-error');
+let emailError = document.getElementById('email-error');
+let messageError = document.getElementById('message-error');
 
+
+function validateName(){
+    let name = document.getElementById('contact-name').value;
+
+    if(name.length == 0) {
+        nameError.innerHTML = 'Full Name is required';
+        return false;
+    }
+
+    if(!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)){ // this code says if name doesn't include letters and 1 space it is wrong
+        nameError.innerHTML = 'Write full name';
+        return false;
+    }
+    nameError.innerHTML = 'valid';
+    return true;
+}
+
+
+
+function validatePhone(){
+    let phone = document.getElementById('').value;
+}
+
+
+
+function validateEmail(){
+    let email = document.getElementById('').value;
+}
+
+
+
+function validateMessage(){
+    let message = document.getElementById('').value;
+}
